8ca59c7c7a0604f3fbffa2f10bfa30d4f0c234d5
paraselene92 <paraselene92@gmail.com>
2024-11-17 11:55:05 +0900

    mod: Unsubscribing from Events

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -6,7 +6,8 @@ function App() {
   const [count, setCount] = useState(0)
 
   useEffect(() => {
-    window.electron.subscribeStatistics((stats) => console.log(stats));
+    const unsub = window.electron.subscribeStatistics((stats) => console.log(stats));
+    return unsub
   }, []);
 
   return (
